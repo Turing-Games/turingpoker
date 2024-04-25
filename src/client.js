f1f8e3a7e29e2b06460224cc37d206a7b05bc157
Jarrod  Luca <jarrodluca@jarrods-air.mynetworksettings.com>
2024-04-24 23:12:52 -0400

    refacgtor to accomodate more games in future in UI

--- src/client.js
+++ src/client.js
@@ -4,6 +4,7 @@ import PartySocket from "partysocket";
 import header from "./components/header";
 import { getImagePath } from "./utils/string_utilities";
 import card from './components/card'
+import poker from "./components/poker";
 
 const gameState = {
   isConnected: false,
@@ -59,135 +60,18 @@ const gameState = {
   }
 };
 
-const GameControls = {
-  view: () => {
-    // Do not display controls until the game state is fully received and the game is active
-    console.log("here is the bug")
-    console.log(gameState.isConnected)
-    console.log(!gameState.gameData)
-    if (!gameState.isConnected || !gameState.gameData) {
-      return m("p", "Waiting for the game to start or connect...");
-    }
-
-    // Determine if it's the current player's turn
-    console.log(gameState.gameData.currentPlayer)
-    console.log(gameState.playerId)
-
-    // Retrieve the current bet and minimum raise amount
-    const currentBet = gameState.gameData.bettingRound.currentBet;
-    const minRaiseAmount = currentBet > 0 ? currentBet + gameState.gameData.bigBlind : gameState.gameData.bigBlind;
-
-    // Render game controls if it's the current player's turn
-    return m("div", [
-      currentBet > 0 ? m("button", {
-        onclick: () => gameState.sendAction("call", currentBet)
-      }, "Call") : null,
-      m("button", {
-        onclick: () => gameState.sendAction("check"),
-        disabled: currentBet > 0
-      }, "Check"),
-      m("button", {
-        onclick: () => {
-          const amount = prompt(`Enter amount to raise (minimum: $${minRaiseAmount}):`, minRaiseAmount);
-          if (amount && parseInt(amount, 10) >= minRaiseAmount) {
-            gameState.sendAction("raise", parseInt(amount, 10));
-          } else {
-            alert(`Invalid raise amount. You must raise at least $${minRaiseAmount}.`);
-          }
-        }
-      }, "Raise"),
-      m("button", {
-        onclick: () => gameState.sendAction("fold")
-      }, "Fold")
-    ]);
-  }
-};
-
 const App = {
   oninit: gameState.connect,
   view: () => {
-
-    console.log(gameState?.gameData?.players)
-    const currentPlayer = gameState?.gameData?.players?.find(player => player.playerId === gameState?.playerId)
-    const isCurrentPlayerTurn = currentPlayer?.playerId === gameState?.gameData?.players[gameState.gameData.currentPlayer].playerId;
-
-    const gameOverview = [
-      { label: 'Current Pot:', value: gameState?.gameData?.potTotal, prefix: '$' },
-      { label: 'Current Bet:', value: gameState?.gameData?.bettingRound?.currentBet, prefix: '$' },
-      { label: 'Dealer Position:', value: gameState?.gameData?.dealerPosition + 1, prefix: '' }
-    ]
-
-    if (!gameState.gameData) {
-      return m("p", "Loading...");
-    }
-
-    console.log({ currentPlayer })
-
-    return m.fragment([
-      // header
-      m(header, {
-        gameType: gameState.gameData.gameType
-      }),
-      // game overview data
-      typeof gameState.gameData === "string" ?
-        m("p", gameState.gameData) :
-        m("div.tg-poker__overview",
-          gameOverview.map((stat, i) => {
-            return (
-              m("div", { style: { color: "#5cc133" } }, [
-                m("div", stat.label),
-                m("div", `${stat.prefix}${stat.value}`)
-              ])
-            )
-          })
-        ),
-      m("div.tg-poker__table", [
-        // other players
-        m("div.tg-poker__table__top",
-          // filter out current player
-          gameState.gameData.players.filter(player => player.playerId !== currentPlayer?.playerId).map((player, index) =>
-            m("div.tg-poker__player", {
-              class: gameState.playerId === player.playerId ? 'current-player' : ''
-            }, [
-              m("h4", `Player ${index + 2} (${player.status}) ${player.playerId === gameState.gameData.players[gameState.gameData.currentPlayer].playerId ? ' - Your Turn' : ''}`),
-              m("div", `Stack: $${player.stackSize}`),
-              m("div", `Current Bet: $${player.currentBet}`),
-              m("div", `Cards: ${player.cards.join(', ')}`),
-              gameState.playerId === player.playerId ? m("div", "This is You") : null
-            ])
-          ),
-        ),
-        // current player and spectators
-        currentPlayer &&
-        m("div.tg-poker__table__bottom", [
-          m("div.tg-poker__player", [
-            m("h4", `You (${currentPlayer?.status}) ${isCurrentPlayerTurn ? ' - Your Turn' : ''}`),
-            m("div", `Stack: $${currentPlayer?.stackSize}`),
-            m("div", `Current Bet: $${currentPlayer?.currentBet}`),
-            m("div", {
-              style: { display: 'flex', gap: '6px' }
-            },
-              currentPlayer?.cards.map((c, i) => {
-                return m(card, { value: c.value, style: { height: '80px' } })
-              })
-            )
-          ]),
-
-          m("div.tg-poker__table__spectators", [
-            m("h4", "Spectators"),
-            gameState.gameData.spectators.map((spectator, index) =>
-              m("div.tg-poker__table__spectators__spectator", [
-                m("p", `Spectator ${index + 1}:`),
-                m("p", `${spectator.status}`)
-              ])
-            )
-          ])
-        ]),
-        isCurrentPlayerTurn ?
-          m(GameControls) :
-          m("p", "Waiting for your turn...")
-      ]),
-    ]);
+    return (
+      m.fragment([
+        // header
+        m(header, {
+          gameType: gameState?.gameData?.gameType
+        }),
+        m(poker, { gameState })
+      ])
+    )
   }
 };
 
---
+++ src/components/poker.js
@@ -0,0 +1,147 @@
+// Global state management
+
+import PartySocket from "partysocket";
+import { getImagePath } from "../utils/string_utilities";
+import header from "./header";
+import card from "./card";
+
+const GameControls = {
+  view: ({ attrs }) => {
+    //   // Do not display controls until the game state is fully received and the game is active
+    //   console.log("here is the bug")
+    //   console.log(gameState.isConnected)
+    //   console.log(!gameState.gameData)
+    //   if (!gameState.isConnected || !gameState.gameData) {
+    //     return m("p", "Waiting for the game to start or connect...");
+    //   }
+
+    //   // Determine if it's the current player's turn
+    //   console.log(gameState.gameData.currentPlayer)
+    //   console.log(gameState.playerId)
+
+    //   // Retrieve the current bet and minimum raise amount
+    //   const currentBet = gameState.gameData.bettingRound.currentBet;
+    //   const minRaiseAmount = currentBet > 0 ? currentBet + gameState.gameData.bigBlind : gameState.gameData.bigBlind;
+
+    //   // Render game controls if it's the current player's turn
+    //   return m("div", [
+    //     currentBet > 0 ? m("button", {
+    //       onclick: () => gameState.sendAction("call", currentBet)
+    //     }, "Call") : null,
+    //     m("button", {
+    //       onclick: () => gameState.sendAction("check"),
+    //       disabled: currentBet > 0
+    //     }, "Check"),
+    //     m("button", {
+    //       onclick: () => {
+    //         const amount = prompt(`Enter amount to raise (minimum: $${minRaiseAmount}):`, minRaiseAmount);
+    //         if (amount && parseInt(amount, 10) >= minRaiseAmount) {
+    //           gameState.sendAction("raise", parseInt(amount, 10));
+    //         } else {
+    //           alert(`Invalid raise amount. You must raise at least $${minRaiseAmount}.`);
+    //         }
+    //       }
+    //     }, "Raise"),
+    //     m("button", {
+    //       onclick: () => gameState.sendAction("fold")
+    //     }, "Fold")
+    //   ]);
+  }
+};
+
+export default {
+  oninit: () => false,
+  view: ({ attrs }) => {
+    const gameState = attrs.gameState;
+
+    console.log(gameState?.gameData?.players)
+    const currentPlayer = gameState?.gameData?.players?.find(player => player.playerId === gameState?.playerId)
+    const isCurrentPlayerTurn = currentPlayer?.playerId === gameState?.gameData?.players[gameState.gameData.currentPlayer].playerId;
+
+    const gameOverview = [
+      { label: 'Current Pot:', value: gameState?.gameData?.potTotal, prefix: '$' },
+      { label: 'Current Bet:', value: gameState?.gameData?.bettingRound?.currentBet, prefix: '$' },
+      { label: 'Dealer Position:', value: gameState?.gameData?.dealerPosition + 1, prefix: '' }
+    ]
+
+    if (!gameState.gameData) {
+      return m("p", "Loading...");
+    }
+
+    console.log({ currentPlayer })
+
+    return m("div.tg-poker__table",
+      [
+        // other players
+        m("div.tg-poker__table__top",
+          // game overview data
+          typeof gameState.gameData === "string" ?
+            m("p", gameState.gameData) :
+            m("div.tg-poker__overview",
+              gameOverview.map((stat, i) => {
+                return (
+                  m("div", { style: { color: "#5cc133" } }, [
+                    m("div", stat.label),
+                    m("div", `${stat.prefix}${stat.value}`)
+                  ])
+                )
+              })
+            ),
+          // filter out current player
+          gameState.gameData.players.filter(player => player.playerId !== currentPlayer?.playerId).map((player, index) => {
+            console.log({ player })
+            return (
+              m("div.tg-poker__player", {
+                class: gameState.playerId === player.playerId ? 'current-player' : ''
+              }, [
+                m("h4", `Player ${index + 2} (${player.status}) ${player.playerId === gameState.gameData.players[gameState.gameData.currentPlayer].playerId ? ' - Your Turn' : ''}`),
+                m("div", `Stack: $${player.stackSize}`),
+                m("div", `Current Bet: $${player.currentBet}`),
+                m("div", `Cards: ${player.cards.join(', ')}`),
+                m("div", {
+                  style: { display: 'flex', gap: '6px' }
+                },
+                  player?.cards.map((c, i) => {
+                    return m(card, { value: c.value, style: { height: '80px' } })
+                  })
+                )
+              ])
+            )
+          }),
+        ),
+        // current player and spectators
+        currentPlayer &&
+        m("div.tg-poker__table__bottom", [
+          m("div.tg-poker__player.tg-poker__player--1", [
+            m("div.tg-poker__player__details", [
+              m("h4", `You (${currentPlayer?.status}) ${isCurrentPlayerTurn ? ' - Your Turn' : ''}`),
+              m("div", [
+                m("div", `$${currentPlayer?.stackSize}`),
+                m("div", `Bet: $${currentPlayer?.currentBet}`),
+              ])
+            ]),
+            m("div", {
+              style: { display: 'flex', gap: '6px' }
+            },
+              currentPlayer?.cards.map((c, i) => {
+                return m(card, { value: c.value, style: { height: '80px' } })
+              })
+            ),
+            isCurrentPlayerTurn ?
+              m(GameControls) :
+              m("p", "Waiting for your turn...")
+          ]),
+
+          m("div.tg-poker__table__spectators", [
+            m("h4", "Spectators"),
+            gameState.gameData.spectators.map((spectator, index) =>
+              m("div.tg-poker__table__spectators__spectator", [
+                m("p", `Spectator ${index + 1}:`),
+                m("p", `${spectator.status}`)
+              ])
+            )
+          ]),
+        ]),
+      ]);
+  }
+};
