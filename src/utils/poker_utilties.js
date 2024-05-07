540a3d13741bbc3713321f420bedbae0eff41933
Jarrod  Luca <jarrodluca@Jarrods-Air.localdomain>
2024-05-07 12:08:16 -0400

    utilities

--- src/utils/poker_utilties.js
+++ src/utils/poker_utilties.js
@@ -1,3 +1,9 @@
+/** isBettingRoundComplete
+ * determines if round is complete, all players have called, checked, ord folded
+ * @param {number} bettingRound 
+ * @param {Array} players 
+ * @returns 
+ */
 export const isBettingRoundComplete = (bettingRound, players = []) => {
   const playerRounds = players.map(player => player.completedRound)
   // sum of rounds
@@ -6,6 +12,12 @@ export const isBettingRoundComplete = (bettingRound, players = []) => {
   return (roundSum / players.length) === bettingRound
 }
 
+/** getPlayersWithRemainingCall
+ * finds players with who have yet to call current
+ * betting round, returns player indexes
+ * @param {Array} players 
+ * @returns []
+ */
 export const getPlayersWithRemainingCall = (players = []) => {
   return []
 }
