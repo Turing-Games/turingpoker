195eda36e51dc87f289eb31659e2d70f300032d6
Jarrod  Luca <jarrodluca@jarrods-air.mynetworksettings.com>
2024-05-14 11:19:02 -0400

    debug static files

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,33 +1,36 @@
 import { Hono } from 'hono'
 import { renderer } from './renderer'
+import { serveStatic } from '@hono/node-server/serve-static'
 
 const app = new Hono()
 
 app.get('*', renderer)
+// app.use('/static/*', serveStatic({ root: '/' }))
 
-app.get('/hello', (c) => {
-  return c.json({
-    message: `Hello!`,
-  })
-})
 
-app.get('/test', (c) => {
-  return c.render(
-    <html>
-      <head>
-        {process.env.NODE_ENV == 'production' ? (
-          <script type='module' src='/app/dist/client.js'></script>
-        ) : (
-          <script type='module' src='/app/client.tsx'></script>
-        )}
-      </head>
-      <body>
-        <div id="root"></div>
-        <h1>Hello</h1>
-      </body>
-    </html>
-  )
-})
+// app.get('/hello', (c) => {
+//   return c.json({
+//     message: `Hello!`,
+//   })
+// })
+
+// app.get('/test', (c) => {
+//   return c.render(
+//     <html>
+//       <head>
+//         {process.env.NODE_ENV == 'production' ? (
+//           <script type='module' src='/app/dist/client.js'></script>
+//         ) : (
+//           <script type='module' src='/app/client.tsx'></script>
+//         )}
+//       </head>
+//       <body>
+//         <div id="root"></div>
+//         <h1>Hello</h1>
+//       </body>
+//     </html>
+//   )
+// })
 
 
 export default app
