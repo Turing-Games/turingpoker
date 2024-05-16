26dc7b8b75dea2f86a91c443c550c9f8a65641c5
Jarrod  Luca <jarrodluca@Jarrods-Air.lan>
2024-05-16 13:19:34 -0400

    wip

--- app/src/pages/home.tsx
+++ app/src/pages/home.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import { Link } from 'react-router-dom'
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import Logo from '../../static/images/logo.png'
 
 
 export default function Home() {
   return (
     <header>
+      <Link to='/'>
+        <img src={Logo} style={{ height: 40 }} />
+      </Link>
       <SignedOut>
         <SignInButton />
       </SignedOut>
