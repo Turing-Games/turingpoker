0cbdd49f9285bb42cce3a9ee3cee7d58134401ec
Jarrod  Luca <jarrodluca@Jarrods-MacBook-Air.local>
2024-07-04 23:02:12 -0400

    wip

--- src/constants/partykit.ts
+++ src/constants/partykit.ts
@@ -1,4 +1,7 @@
 const PROD = import.meta?.env?.PROD
+console.log('env...')
+console.log(import.meta?.env)
+console.log(process.env)
 const PARTYKIT_HOST = PROD ? 'ws.turingpoker.com' : 'localhost:1999'
 export const PARTYKIT_PROTOCOL = PROD ? 'https' : 'http'
 export const PARTYKIT_URL = `${PARTYKIT_PROTOCOL}://${PARTYKIT_HOST}`;
