031bf5c0d6d7e463044a10673758ce26713c30e9
Jarrod  Luca <jarrodluca@Jarrods-MacBook-Air.local>
2024-07-08 12:59:52 -0400

    hard code test key

--- app/src/routes/root.tsx
+++ app/src/routes/root.tsx
@@ -20,7 +20,7 @@ export type ClientState = {
   updateLog: ServerUpdateMessage[];
 };
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = 'pk_test_YmVjb21pbmctc2hhcmstMTAuY2xlcmsuYWNjb3VudHMuZGV2JA'
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
