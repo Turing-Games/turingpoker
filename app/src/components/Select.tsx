1a5b3ddbb9ba1ab06e5e73944a5f70c4ab9736a2
Jarrod  Luca <jarrodluca@Jarrods-MacBook-Air.local>
2024-07-16 13:07:25 -0400

    wip

---
+++ app/src/components/Select.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default function Select({
+  options = [],
+  selected = '',
+  onChange = (param) => false,
+  placeholder = 'Select an option',
+}: {
+  options: { label: string, value: string }[],
+  selected: string,
+  onChange: (param: string) => void,
+  placeholder: string
+}) {
+
+  const [option, setOption] = React.useState(options.find(o => o.value === selected)?.value || options[0].value)
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  return (
+    <div>
+      <div
+        className="p-[8px] cursor-pointer border rounded-lg w-full h-[40px]"
+        onClick={() => setIsOpen(!isOpen)}
+      >{option?.label || placeholder}
+      </div>
+      {isOpen &&
+        <div className="bg-white border absolute top-[45px] left-0 shadow-md rounded-lg w-full">
+          {options.map(option => (
+            <div
+              key={option.value}
+              className="w-full p-[8px] hover:bg-gray-100 cursor-pointer w-full"
+              onClick={() => {
+                setOption(option)
+                if (onChange) {
+                  onChange(option.value)
+                }
+                setIsOpen(false)
+              }}
+            >{option.label}</div>
+          ))}
+        </div>
+      }
+    </div>
+  )
+}
--- app/src/pages/home.tsx
+++ app/src/pages/home.tsx
@@ -3,12 +3,12 @@ import GameClient from '@app/components/GameClient';
 import Main from '@app/layouts/main';
 import { useParams } from 'react-router-dom';
 import LogoDark from '@static/images/logo-dark.png';
+import Select from '@app/components/Select';
 
 
 export default function Home() {
 
   const [game, setGame] = React.useState<string>('');
-  const [isOpen, setIsOpen] = React.useState<boolean>(false);
   const [join, setJoin] = React.useState<boolean>(false);
   let { gameId } = useParams();
 
@@ -27,25 +27,7 @@ export default function Home() {
           <div className="bg-white p-[16px] border flex flex-col items-center justify-center w-full max-w-[300px] m-auto">
             <img src={LogoDark} className="w-[150px] mx-auto mb-[32px]" />
             <div className="relative w-full max-w-[250px]">
-              <div
-                className="p-[8px] cursor-pointer border rounded-lg w-full h-[40px]"
-                onClick={() => setIsOpen(!isOpen)}
-              >{game?.label || 'Select a game to start:'}
-              </div>
-              {isOpen &&
-                <div className="bg-white border absolute top-[45px] left-0 shadow-md rounded-lg w-full">
-                  {games.map(game => (
-                    <div
-                      key={game.value}
-                      className="w-full p-[8px] hover:bg-gray-100 cursor-pointer w-full"
-                      onClick={() => {
-                        setGame(game)
-                        setIsOpen(false)
-                      }}
-                    >{game.label}</div>
-                  ))}
-                </div>
-              }
+              <Select options={games} selected={game} onChange={(value) => setGame(value)} placeholder="Select a game" />
               <button className="w-full mt-[16px]" onClick={() => setJoin(true)}>Start</button>
             </div>
           </div>
