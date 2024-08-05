79962951241cc25c762f736f9b794bba05cdb55f
Jarrod  Luca <jarrodluca@Jarrods-MacBook-Air.local>
2024-08-05 13:59:13 -0400

    add date migration

--- app/migrations/0016_AddCreatedAtToResources.sql
+++ app/migrations/0016_AddCreatedAtToResources.sql
@@ -1,1 +1,8 @@
 -- Migration number: 0016 	 2024-08-05T17:17:08.583Z
+ALTER TABLE users ADD COLUMN created_at TEXT;
+ALTER TABLE games ADD COLUMN created_at TEXT;
+ALTER TABLE tournaments ADD COLUMN created_at TEXT;
+ALTER TABLE api_keys ADD COLUMN created_at TEXT;
+ALTER TABLE game_users ADD COLUMN created_at TEXT;
+ALTER TABLE tournament_configs ADD COLUMN created_at TEXT;
+ALTER TABLE game_configs ADD COLUMN created_at TEXT;
