0c42bb50bf1848b5d1ab39b31fe3b8f22c0d63ef
Jarrod  Luca <jarrodluca@jarrods-air.mynetworksettings.com>
2024-08-16 12:17:35 -0400

    wip

--- app/src/pages/how.tsx
+++ app/src/pages/how.tsx
@@ -12,39 +12,26 @@ export default function How() {
 
   return (
     <Main pageTitle='Learn'>
-      <div className="pt-[48px] px-[20px]">
+      <div className="pt-[48px] px-[20px] max-w-[750px] mx-auto">
         <div
           className="flex items-center gap-[8px] mb-[16px]"
-          // onClick={() => openInfo === 'why' ? setOpenInfo('') : setOpenInfo('why')}
         >
-          {/* <PlusIcon
-            className='transition duration-300'
-            style={{
-              transform: openInfo === 'why' ? 'rotate(45deg)' : 'rotate(0deg)'
-            }}
-          /> */}
           <Heading size="6">Why Are Turing Games Important? </Heading>
         </div>
-        <div
-        // className='transition duration-300 overflow-hidden'
-        // style={{
-        //   height: openInfo === 'why' ? 'auto' : 0,
-        // }}
-        >
-          <div className="block mb-[32px] ">
+        <div>
+          <div className="block mb-[32px]">
             <p className="mb-[16px]">We believe the next great strategists won’t be marshaling fellow humans like Clausewitz or Napoleon, but designing and directing AI based models and agents in a variety of fields of operations, both in kinetic warfare and cyberspace territories.  Poker is an excellent test bed for developing skills and intuition regarding applied game theory and adversarial AI development, which is why we have started here, but the sky is the limit. We’re looking to find, develop, and work with the first true generation of adversarial AI developers and strategists. If you think that could be you, introduce yourself in out Discord.</p>
           </div>
         </div>
 
 
-        <div className="flex items-center gap-[8px] mb-[16px]">
+        <div
+          className="flex items-center gap-[8px] mb-[16px]"
+        >
           <Heading size="4">How it works</Heading>
         </div>
         <div
           className='transition duration-300 overflow-hidden'
-          style={{
-            // height: openInfo === 'how' ? 'auto' : 0,
-          }}
         >
           <div className="block mb-[32px] ml-[8px] pl-[16px] border-black border-l">
             <p className="mb-[16px]">Turing poker is a platform for playing web based, AI poker games, both in standalone and tournament format. To accomplish this a Webscoket connection is established between the Turing Games server, and the device running the AI model. The websocket stream passes game state data to the model or algorithm running in the user device, while accepting valid action messages to facilitate game participation.</p>
@@ -52,15 +39,12 @@ export default function How() {
         </div>
 
 
-        <div className="flex items-center gap-[8px] mb-[16px]">
+        <div
+          className="flex items-center gap-[8px] mb-[16px]"
+        >
           <Heading size="4">Connecting a bot</Heading>
         </div>
-        <div
-          className='transition duration-300 overflow-hidden'
-          style={{
-            // height: openInfo === 'connecting' ? 'auto' : 0,
-          }}
-        >
+        <div className='transition duration-300 overflow-hidden'>
           <div className="block mb-[32px] ml-[8px] pl-[16px] border-black border-l">
             <p className="mb-[16px]">To connect a bot to the game, follow these steps:</p>
             <ol className="list-decimal pl-[32px]">
